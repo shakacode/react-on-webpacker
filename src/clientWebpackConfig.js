3d8634fefa06269f83284cff5ca6923fd743606a
Judah Meek <judah.meek@gmail.com>
2021-11-24 22:29:42 -0600

    move react-refresh config from tut development to RoW clientConfig

--- src/clientWebpackConfig.js
+++ src/clientWebpackConfig.js
@@ -1,7 +1,20 @@
 const commonWebpackConfig = require('./commonWebpackConfig')
+const { inliningCss } = require('@rails/webpacker')
 
 const configureClient = () => {
   const clientConfig = commonWebpackConfig()
+
+  //plugins
+  if (inliningCss ) {
+    const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+    clientConfig.plugins.push(
+      new ReactRefreshWebpackPlugin({
+        overlay:{
+          sockPort: devServer.port
+        }
+      })
+    )
+  }
 
   // server-bundle is special and should ONLY be built by the serverConfig
   // In case this entry is not deleted, a very strange "window" not found
